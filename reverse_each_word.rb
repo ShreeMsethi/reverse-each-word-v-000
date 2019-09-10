d27e7f4d14c57eed2c7813c3840017baa6282f00
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-09-10 03:59:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,11 @@ def reverse_each_word(string)
   a_to_s
 end
 
+def reverse_each_word(string)
+  s_to_a = string.split
+  array_reversed = []
+  s_to_a.collect {|i| array_reversed << i.reverse }
+  a_to_s = array_reversed.join(" ")
+  a_to_s
+end
+
